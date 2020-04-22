e3b689bcbfcf63437a938c754b8c760f7525e301
es-smaha <samahoum14@gmail.com>
2020-04-22 16:13:35 +0000

    navbar

--- resources/views/agent/about.blade.php
+++ resources/views/agent/about.blade.php
@@ -44,32 +44,9 @@
 							<li><a href="sidebar-left.html">Left Sidebar</a></li>
 							<li><a href="sidebar-right.html">Right Sidebar</a></li>
 						</ul>
-          </li>
-          <li class="dropdown">
-          <li class="nav-item dropdown">
-        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-            {{ Auth::user()->name }} <span class="caret"></span>
-        </a>
-				
-          
-
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-        
-            <a class="dropdown-item" href="{{ route('logout') }}"
-                onclick="event.preventDefault();
-                                document.getElementById('logout-form').submit();">
-                {{ __('Logout') }}
-            </a>
-
-            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                @csrf
-            </form>
-        </div>
-  
-      </li>
-				
+					</li>
 					<li><a href="contact.html">Contact</a></li>
-			
+					<li><a class="btn" href="signin.html">SIGN IN / SIGN UP</a></li>
 				</ul>
 			</div><!--/.nav-collapse -->
 		</div>
@@ -179,9 +156,8 @@
 								<a href="#">Home</a> | 
 								<a href="about.html">About</a> |
 								<a href="sidebar-right.html">Sidebar</a> |
-                <a href="contact.html">Contact</a> 
-
-
+								<a href="contact.html">Contact</a> |
+								<b><a href="signup.html">Sign up</a></b>
 							</p>
 						</div>
 					</div>
